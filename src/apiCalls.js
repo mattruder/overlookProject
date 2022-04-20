97d2c9eaaf2346bd2316d65e0a67ed027997d642
Matt Ruder <matt.j.ruder@gmail.com>
2022-04-20 08:28:36 -0600

    Add imports to tests and basic API calls

---
+++ src/apiCalls.js
@@ -0,0 +1,31 @@
+let customers = `http://localhost:3001/api/v1/customers`
+let rooms = `	http://localhost:3001/api/v1/rooms`
+let bookings = `http://localhost:3001/api/v1/bookings`
+
+
+
+const getData = () => {
+    return Promise.all([
+        fetchCustomerData(),
+        fetchRoomsData(),
+        fetchBookingsData()
+    ])
+}
+
+const fetchCustomerData = () => {
+    return fetch(customers)
+    .then(response => response.json())
+    .catch(err => console.log('ERROR'))
+}
+
+const fetchRoomsData = () => {
+    return fetch(rooms)
+    .then(response => response.json())
+    .catch(err => console.log('ERROR'))
+}
+
+const fetchBookingsData = () => {
+    return fetch(bookings)
+    .then(response => response.json())
+    .catch(err => console.log('ERROR'))
+}
